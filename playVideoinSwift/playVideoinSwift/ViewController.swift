ad8b3e7871859d60b93d7a35d89ebfa285fd7d3d
Rohim-dev <rohim.dev@gmail.com>
2015-01-08 16:07:47 +0300

    iOS App Developemnt
    iOS App development

---
+++ playVideoinSwift/playVideoinSwift/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  playVideoinSwift
+//
+//  Created by Rohim Developer on 1/8/15.
+//  Copyright (c) 2015 Smart HRD. All rights reserved.
+//
+
+
+
+import UIKit
+import MediaPlayer
+
+
+class ViewController: UIViewController {
+    var moviePlayer: MPMoviePlayerController?
+    
+    
+    func stopPlayingVideo() {
+        if let player = moviePlayer{
+            
+            NSNotificationCenter.defaultCenter().removeObserver(self)
+            player.stop()
+            player.view.removeFromSuperview()
+            
+        }
+        
+        
+    }
+    
+    
+    func startPlayingVideo(){
+        
+        let mainBundle = NSBundle.mainBundle()
+        let url = mainBundle.URLForResource("video", withExtension: "mp4")
+        
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        if let player = moviePlayer{
+            
+            NSNotificationCenter.defaultCenter().addObserver(self,
+                selector: "stopPlayingVideo",
+                name: MPMoviePlayerPlaybackDidFinishNotification,
+                object: nil)
+            
+            player.scalingMode = .AspectFit
+            view.addSubview(player.view)
+            player.setFullscreen(true, animated: false)
+            
+            //start play
+            player.play()
+        } else {
+            println("Failed to instantiate the movie player")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func playBtn(sender: AnyObject) {
+        
+         startPlayingVideo()
+    }
+    
+}
